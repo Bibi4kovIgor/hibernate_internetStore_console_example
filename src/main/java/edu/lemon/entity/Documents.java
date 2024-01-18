f04ed660e11b87b0352c66e574dc5bae7b837e4b
ihor <work.bibi4kov@gmail.com>
2024-01-18 03:48:58 +0200

    Fixed warning:   - Removed unused import

--- src/main/java/edu/lemon/entity/Documents.java
+++ src/main/java/edu/lemon/entity/Documents.java
@@ -1,9 +1,11 @@
 package edu.lemon.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Objects;
 import java.util.UUID;
 
 @Data
